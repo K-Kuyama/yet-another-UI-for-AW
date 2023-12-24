8645b7fdc3877003a5c6ab725f8b3e4c8cac1978
Kazuhiro Kuyama <kuyama@systemdesignk2.jp>
2023-12-24 18:41:34 +0900

    Change sample deta base

--- aw_ya_core/datastore.py
+++ aw_ya_core/datastore.py
@@ -1,9 +1,10 @@
 import sqlite3
 import pandas as pd
 
+
 class DataStore:
     _instance = None
-    
+
     def __init__(self):
         self.db_name = "DefDB.db"
         self.views_table = "AnalysisViewDef"
@@ -27,27 +28,26 @@ class DataStore:
             cur.execute(
                 (f"CREATE TABLE IF NOT EXISTS {self.words_table}"
                  f"(id INTEGER PRIMARY KEY,category_id INTEGER, word TEXT,positive INTEGER)")
-            )            
+            )
             conn.commit()
-        
+
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
 
-    
     def clear(self):
         with sqlite3.connect(self.db_name) as conn:
             cur = conn.cursor()
             cur.execute(f"DELETE from {self.words_table}")
-            cur.execute(f"DELETE from {self.events_table}")            
+            cur.execute(f"DELETE from {self.events_table}")
             cur.execute(f"DELETE from {self.categories_table}")
             cur.execute(f"DELETE from {self.views_table}")
             conn.commit()
 
     def query(self, query_str):
-        with sqlite3.connect(self.db_name) as conn:  
-            df=pd.read_sql_query(query_str, conn)
+        with sqlite3.connect(self.db_name) as conn:
+            df = pd.read_sql_query(query_str, conn)
         def_list = df.to_numpy().tolist()
         return def_list
 
@@ -61,24 +61,23 @@ class DataStore:
         return rid
 
     def inputTestData(self):
-        #テスト用初期データ
-        datas =[{"view":["作業オーダ","Blue",0],
-        "categories":[{"category":["EN0001",'#0000FF'],
-                        "words":[["PMBOK", True],["エンジニアリング", True],
-                                ["マネージメント", True],["タイムトラッキング", False]]},
-                         {"category":["DX0002","#6A80FF"],
-                          "words":[["Jupyter", True],["システム",True],["マネージメント",False]]}]
-            },
-        {"view":["作業分類","Green",0],
-             "categories":[{"category":["文書作成",'#0000FF'],
-                         "words":[["メモ", True],["Word", True],
-                                ["新規作成", True]]},
-                          {"category":["管理作業","#6A80FF"],
-                          "words":[["freee", True],["コンソール",True]]                        
-                          }]
-            }]
-        
-        #初期データをデータベースに格納する
+        # テスト用初期データ
+        datas = [{"view": ["作業オーダ", "Blue", 0],
+                  "categories": [{"category": ["EN0001", '#0000FF'],
+                                  "words": [["PMBOK", True], ["エンジニアリング", True],
+                                            ["マネージメント", True], ["タイムトラッキング", False]]},
+                                 {"category": ["DX0002", "#6A80FF"],
+                                  "words": [["Jupyter", True], ["システム", True], ["マネージメント", False]]}]
+                  },
+                 {"view": ["作業分類", "Green", 0],
+                  "categories": [{"category": ["文書作成", '#0000FF'],
+                                  "words": [["メモ", True], ["Word", True],
+                                            ["新規作成", True]]},
+                                 {"category": ["管理作業", "#6A80FF"],
+                                  "words": [["freee", True], ["コンソール", True]]}]
+                  }]
+
+        # 初期データをデータベースに格納する
         with sqlite3.connect(DataStore().db_name) as conn:
             cur = conn.cursor()
             for data in datas:
@@ -94,7 +93,7 @@ class DataStore:
                     cur.execute(query_str+" RETURNING id")
                     cid = cur.fetchone()
                     for winfo in cdata["words"]:
-#                        print(winfo)
+                        # print(winfo)
                         x = 1 if winfo[1] else 0
                         query_str = f"INSERT into {DataStore().words_table} (category_id, word,positive) values ({cid[0]},'{winfo[0]}',{x})"
 #                        print(query_str)
