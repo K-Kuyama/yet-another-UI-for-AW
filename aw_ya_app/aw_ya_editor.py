66addf4c0a367ccb6c07d3550cca8f00d017178e
Kazuhiro Kuyama <kuyama@systemdesignk2.jp>
2024-01-17 18:38:42 +0900

    Re setup launcher app

--- aw_ya_app/aw_ya_editor.py
+++ aw_ya_app/aw_ya_editor.py
@@ -3,12 +3,11 @@ Created on 2024/01/12
 
 @author: kuyamakazuhiro
 '''
-import subprocess
-import signal
-import time
+
+
 import sys
 import os
-import threading
+
 from voila.app import Voila
 
 
@@ -23,60 +22,8 @@ def start_voila(file_path):
     app.initialize([file_path])
     app.start()
 
-def main():
 
-    wait_time = 5    
-    rpath = get_resource_path()
-    os.environ['YA_DBFILE_PATH']=os.path.join(rpath,"DefDB.db")
+rpath = get_resource_path()
+os.environ['YA_DBFILE_PATH']=os.path.join(rpath,"DefDB.db")
+start_voila(os.path.join(rpath,"DefEditorApp.ipynb")) 
     
-#    tr1 = threading.Thread(target=start_voila, args=[os.path.join(rpath,"DefEditorApp.ipynb")],
-#                     daemon=True)
-#    tr1.start()
-    start_voila(os.path.join(rpath,"DefEditorApp.ipynb")) 
-    
-    time.sleep(wait_time)
-        
-#    tr2 = threading.Thread(target=start_voila, args=[os.path.join(rpath,"QtDashboardApp.ipynb")],
- #                    daemon=True)
-#    tr2.start()  
-    start_voila(os.path.join(rpath,"QtDashboardApp.ipynb")) 
-    print("Ctrl-C for exit")
-    while True:
-#        print(f"waiting {wait_time} sec")
-        time.sleep(wait_time)    
-
-
-'''
-def main():
-    print("main")
-
-    def handler(signum, frame):
-        if p1:
-            print(f"send kill to {p1}")
-            p1.kill()        
-        if p2:
-            print(f"send kill to {p2}")
-            p2.kill()
-        exit()
-        
-    signal.signal(signal.SIGINT, handler)
-    
-    wait_time = 5
-    
-    rpath = get_resource_path()
-
-    os.environ['YA_DBFILE_PATH']=os.path.join(rpath,"DefDB.db")
-    
-    command_1 = ["voila", os.path.join(rpath,"DefEditorApp.ipynb")]
-    command_2 = ["voila", os.path.join(rpath,"QtDashboardApp.ipynb")]
-
-    p1 = subprocess.Popen(command_1)
-    print(p1)
-    time.sleep(wait_time)
-    p2 = subprocess.Popen(command_2)
-    print(p2)
-
-    while True:
-        print(f"waiting {wait_time} sec")
-        time.sleep(wait_time)
-'''
